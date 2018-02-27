b7a7328948c2a41736c0969286199a324cd6689c
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-02-27 19:30:59 +0200

    Added timeout

--- pb-titler.lua
+++ pb-titler.lua
@@ -9,7 +9,7 @@ on('PRIVMSGEvent', function(e)
   end
   local url = string.match(e.message, '(https?://[%d%w%.%/%%%&%?]+%.[%d%w%.%/%%%&%?]+)')
   if url ~= nil then
-    local headers, stream = http.new_from_uri(url):go()
+    local headers, stream = http.new_from_uri(url):go(5)
     if headers == nil then
       reply(e, e.nick .. ' -> Unable to get ' .. url .. '!')
       return
