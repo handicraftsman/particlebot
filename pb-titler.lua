5a572c89aea6a17e6d9f72846f9db11f6e448a44
Gogs <gogs@fake.local>
2018-03-01 11:29:37 -0500

    Updated URL search pattern

--- pb-titler.lua
+++ pb-titler.lua
@@ -7,7 +7,7 @@ on('PRIVMSGEvent', function(e)
   else
     reply_to = e.target
   end
-  local url = string.match(e.message, '(https?://[%d%w%.%_-%+=#/%%%&%?]+%.[%d%w%.%_-%+=#/%%%&%?]+)')
+  local url = string.match(e.message, '(https?://[%d%w%.%_-%+=#/%%%&%?]+%.[%d%w%.%_-%+=#/%%%&%?]+)[^ ]*')
   if url ~= nil then
     local headers, stream = http.new_from_uri(url):go(5)
     if headers == nil then
@@ -21,7 +21,7 @@ on('PRIVMSGEvent', function(e)
     if headers:get(':status') == '200' then
       local title = string.match(body, [[<title>([^<]+)</title>]])
       if title ~= nil then
-        reply(e, e.nick .. ' -> ' .. title:gsub("&nbsp;", ""):gsub("&quot;", ""))
+        reply(e, e.nick .. ' -> ' .. title:gsub("&nbsp;", ""):gsub("&quot;", ""):gsub("\n", " "):sub(0, 100))
       else
         reply(e, e.nick .. ' -> ' .. headers:get(':status') .. ' ' .. headers:get('content-type'))
       end
