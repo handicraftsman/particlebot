3272855bc4cf8f8e54228986627117f48be0d367
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-03-14 16:55:51 +0200

    Updated cookie command

--- fun/main.cpp
+++ fun/main.cpp
@@ -31,55 +31,84 @@ static Iter select_randomly(Iter start, Iter end) {
 }
 
 void cmdfunc_cookie(PB::CommandEvent* event) noexcept {
-  static std::vector<std::string> qualities {
-    "",
-    "%C?LBLUEuncommon ",
-    "%C?BLUErare ",
-    "%C?PURPLEepic ",
-    "%C?YELLOWlegendary ",
-    "%C?ORANGEholy ",
-    "%C?CYANhi-tech ",
-    "%C?LBLUEquantum ",
-    "%C?BLACKevil ",
-    "%C?PURPLEmagical ",
-    "%C?LBLUEancient ",
-    "%C?REDvampiric ",
+  static std::map<std::string, std::string> qualities {
+    {"normal", ""},
+    {"uncommon", "%C?LBLUEuncommon "},
+    {"rare", "%C?BLUErare "},
+    {"epic", "%C?PURPLEepic "},
+    {"legendary", "%C?YELLOWlegendary "},
+    {"holy", "%C?ORANGEholy "},
+    {"hi-tech", "%C?CYANhi-tech "},
+    {"quantum", "%C?LBLUEquantum "},
+    {"evil", "%C?BLACKevil "},
+    {"magical", "%C?PURPLEmagical "},
+    {"ancient", "%C?LBLUEancient "},
+    {"vampiric", "%C?REDvampiric "}
   };
 
-  static std::vector<std::string> types {
-    "",
-    "%C?ORANGEblazing ",
-    "%C?REDhot ",
-    "%C?CYANfrost ",
-    "%C?LBLUEchilling ",
-    "%C?YELLOWshocking ",
-    "%C?LGREYaerial ",
-    "%C?GREYstone ",
-    "%C?BROWNmud ",
-    "%C?BLACKvoid ",
-    "%C?WHITEghostly ",
-    "%C?REDbloody ",
-    "%C?REDn%C?GREENy%C?BLUEa%C?CYANn ",
-    "%C?CYANteleporting ",
-    "%C?BROWNwild ",
-    "%C?GREENalien ",
-    "%C?BLACKspacious ",
-    "%C?REDatomic ",
-    "%C?BROWNchocolate "
+  static std::map<std::string, std::string> types {
+    {"normal", ""},
+    {"blazing", "%C?ORANGEblazing "},
+    {"hot", "%C?REDhot "},
+    {"frozen", "%C?CYANfrozen "},
+    {"chilling", "%C?LBLUEchilling "},
+    {"shocking", "%C?YELLOWshocking "},
+    {"aerial", "%C?LGREYaerial "},
+    {"stone", "%C?GREYstone "},
+    {"mud", "%C?BROWNmud "},
+    {"void", "%C?BLACKvoid "},
+    {"ghostly", "%C?WHITEghostly "},
+    {"bloody", "%C?REDbloody "},
+    {"nyan", "%C?REDn%C?GREENy%C?BLUEa%C?CYANn "},
+    {"teleporting", "%C?CYANteleporting "},
+    {"wild", "%C?BROWNwild "},
+    {"alient", "%C?GREENalien "},
+    {"spacious", "%C?BLACKspacious "},
+    {"atomic", "%C?REDatomic "},
+    {"chocolate", "%C?BROWNchocolate" }
   };
 
-  if (event->split.size() > 1) {
-    event->nreply("Error: invalid argument amount");
-    return;
+  std::string target;
+  std::string quality;
+  std::string type;
+
+  bool has_target = false;
+  bool has_quality = false;
+  bool has_type = false;
+
+  for (std::string s : event->split) {
+    if (s.substr(0,9) == "--quality") {
+      quality = qualities[s.substr(10, s.size())];
+      has_quality = true;
+      continue;
+    }
+    if (s.substr(0,6) == "--type") {
+      type = types[s.substr(7, s.size())];
+      has_type = true;
+      continue;
+    }
+    if (!target.empty())
+      target += " ";
+    target += s;
+    has_target = true;
   }
 
-  std::string quality = *select_randomly(qualities.begin(), qualities.end());
-  std::string type = *select_randomly(types.begin(), types.end());
+  if (!has_quality)
+    quality = select_randomly(qualities.begin(), qualities.end())->second;
+  if (!has_type)
+    type = select_randomly(types.begin(), types.end())->second;
+  if (!has_target) {
+    if (event->reply_to == event->socket->nick) {
+      target = event->target;
+    } else {
+      target = event->nick;
+    }
+  }
 
   if (event->split.empty()) {
-    event->socket->ctcp(event->reply_to, "ACTION &Ngives &B" + event->nick + "&N a &B" + quality + type + "&N&B%C?BROWNcookie&N");
+    event->socket->ctcp(event->reply_to, "ACTION &Ngives &B" + target + "&N a &B" + quality + type + "&N&B%C?BROWNcookie&N");
   } else {
-    event->socket->ctcp(event->reply_to, "ACTION &Ngives &B" + event->split[0] + "&N a &B" + quality + type + "&N&B%C?BROWNcookie&N");
+    event->socket->ctcp(event->reply_to, "ACTION &Ngives &B" + target + "&N a &B" + quality + type + "&N&B%C?BROWNcookie&N");
   }
 }
 
@@ -91,7 +120,7 @@ extern "C" __attribute__((used)) bool pb_init(PB::Bot* bot) noexcept {
 
   PB::CommandInfo& cmd_cookie = register_command("fun", "cookie", cmdfunc_cookie);
   cmd_cookie.description = "Gives a cookie to the target";
-  cmd_cookie.usage = "[target]";
+  cmd_cookie.usage = "[target] [--quality=quality] [--type=type]";
   cmd_cookie.cooldown = 5;
 
   return true;
